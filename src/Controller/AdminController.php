05e3d9d3783cbdb31eee9bd45080121f3fca1644
MauquieLouis <34682000+MauquieLouis@users.noreply.github.com>
2019-03-17 16:47:12 +0100

    Debut Formulaire nouveau compte

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,16 +4,44 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\NewUserFormType;
+use App\Entity\User;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin/controlUser", name="admin_UserControl")
+     * @Route("/admin", name="admin")
      */
     public function index()
     {
+        return $this->render('admin/index.html.twig', [
+
+        ]);
+    }
+    
+    /**
+     * @Route("/admin/controlUser", name="admin_UserControl")
+     */
+    public function UserControl()
+    {
         return $this->render('admin/userControl.html.twig', [
-            'controller_name' => 'AdminController',
+            
+        ]);
+    }
+    
+    /**
+     * @Route("/admin/controlUser/newUser", name="admin_UserControl_newUser")
+     */
+    public function newUser()
+    {
+        $form = $this->createForm(NewUserFormType::class);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->redirectToRoute('Home');
+        }  
+        return $this->render('admin/newUser.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/NewUserFormType.php
+++ src/Form/NewUserFormType.php
@@ -4,26 +4,62 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class NewUserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('Nom')
-            ->add('prenom')
-        ;
+        ->add('nom', TextType::class, ['attr'=>['autocomplete' => 'disabled']])
+        ->add('prenom', TextType::class, ['attr'=>['autocomplete' => 'disabled']])
+        ->add('email', EmailType::class, ['attr'=>['autocomplete' => 'disabled']])
+        ->add('password', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'required' => true,
+            'constraints' => array(
+                new NotBlank(),
+                new Length(array('min' => 6)),
+            ),
+            'first_options'  => array('label' => 'label.password'),
+            'second_options' => array('label' => 'label.passwordConfirmation'),
+        ));
+//         ->add('Password', RepeatedType::class, [
+//             // instead of being set onto the object directly,
+//             // this is read and encoded in the controller
+//             'type' => PasswordType::class,
+//             'invalid_message' => 'Les mots de passes doivent êtres similaires.',
+//             'options' => ['attr' => ['class' => 'password-field']],
+//             'required' => true,
+//             'first_options'  => ['label' => 'Mot de passe'],
+//             'second_options' => ['label' => 'Confirmer Mot de passe'],
+//             'mapped' => false,
+//             'constraints' => [
+//                 new NotBlank([
+//                     'message' => 'Please enter a password',
+//                 ]),
+//                 new Length([
+//                     'min' => 6,
+//                     'minMessage' => 'Your password should be at least {{ limit }} characters',
+//                     // max length allowed by Symfony for security reasons
+//                     'max' => 4096,
+//                 ]),
+//             ],
+//         ])
+//         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+             'data_class' => User::class,
         ]);
     }
 }
