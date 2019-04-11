d35b67653750a7d151663a75671822964c4cae38
Mod_loc <Mod_loc@D46-P09.D-Electronique.local>
2019-04-11 17:56:30 +0200

    meilleure gestion du modele 3d

---
+++ src/Form/NewMachineType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Machine;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class NewMachineType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('picturedevant', FileType::class, ['data_class' => null, 'required' => false] )
+            ->add('picturegauche', FileType::class, ['data_class' => null, 'required' => false]  )
+            ->add('picturederriere', FileType::class, ['data_class' => null, 'required' => false]  )
+            ->add('picturedroite', FileType::class, ['data_class' => null, 'required' => false]  )
+            ->add('picturedessus', FileType::class, ['data_class' => null, 'required' => false]  )
+            ->add('picturedessous', FileType::class, ['data_class' => null, 'required' => false]  )
+//             ->add('Enregistrer', SubmitType::class,  array('label' =>'Save Machine'))
+        ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Machine::class,
+        ]);
+    }
+}
--- src/Form/NewMaintenanceType.php
+++ src/Form/NewMaintenanceType.php
@@ -2,26 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Machine;
+use App\Entity\Maintenance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NewMachineFormType extends AbstractType
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class NewMaintenanceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('imagefilename')
+            ->add('nom', TextType::class)
+            ->add('picturefile', FileType::class, ['data_class' => null, 'required' => false] )
+            
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Machine::class,
+            'data_class' => Maintenance::class,
         ]);
     }
 }
