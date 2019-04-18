0200c19c5c56cc732e1acfb896cae1cb4795f38e
Cléo Delprat <cleo.delprat@lycee-bourdelle.fr>
2019-04-18 12:39:42 +0200

    affichage evènement optimisé

--- public/Js/calendarFunctions.js
+++ public/Js/calendarFunctions.js
@@ -51,9 +51,11 @@ return arrD[mm]+((mm==1)?(annBissex?1:0):0);
 
 //----------Evènements----------//
 
-//table.init("event");
-//console.log(table.retour);
-var tabEvent = "";//table.retour;
+var user = table.GetCurrentUser();
+console.log(user);
+var tabEvent = table.init("event");
+console.log(tabEvent);
+//var tabEvent ="";
 var de = new Array();
 var me = new Array();
 var ye = new Array();
@@ -64,20 +66,23 @@ var frequenceYear;
 //alert(maxDays(parseInt(tabEvent[0][4].date.substr(5, 2))-1, parseInt(tabEvent[0][4].date.substr(0, 4))));
 //----Dates evènement----//
 
-for(var j=0; j < parseInt(tabEvent.length); j++){
+for(var j=0; j < 5; j++){
 
-	var frequenceDay = tabEvent[j][6];
+	//var frequenceDay = tabEvent[j][7];
+	//alert(frequenceDay);
 	de[j] = new Array();
 	me[j] = new Array();
 	ye[j] = new Array();
 	frequenceMonth = 0;
 	frequenceYear = 0;
-	de[j][0]=parseInt(tabEvent[j][4].date.substr(8, 2));
-	me[j][0]=parseInt(tabEvent[j][4].date.substr(5, 2));
-	ye[j][0]=parseInt(tabEvent[j][4].date.substr(0, 4));
+	
+	de[j][0]=tabEvent[j][5].date.substr(8, 2);
+	me[j][0]=tabEvent[j][5].date.substr(5, 2);
+	ye[j][0]=tabEvent[j][5].date.substr(0, 4);
+	//alert(de[j][0])
 	//itteration = (nbYear/2*365)/frequenceE*7
 	var l = 0
-	for(var k=1; k < 2; k++, l++){
+	for(var k=1; k <= 1; k++, l++){
 		//alert(me[j][1]);
 		var nbDays = maxDays(parseInt(me[j][l]-1+frequenceMonth), parseInt(ye[j][l]+frequenceYear));
 		//alert(nbDays);
@@ -98,10 +103,12 @@ for(var j=0; j < parseInt(tabEvent.length); j++){
 			me[j][k] = parseInt(me[j][l])+frequenceMonth;
 		}
 		ye[j][k] = parseInt(ye[j][l])+frequenceYear;
+		//alert("jour = " +de[j][k]);
 	}
 	//alert(de[j][1] + "/" + me[j][1] + "/" + ye[j][1] + "---" + de[j][2] + "/" + me[j][2] + "/" + ye[j][2]);
 	//alert(de[j][3] + "/" + me[j][3] + "/" + ye[j][3] + "---" + de[j][4] + "/" + me[j][4] + "/" + ye[j][4]);
 }
+
 //------------------------------//
 
 
@@ -148,14 +155,15 @@ function clickOnCase(id, cal){
 				balise.style.backgroundColor = colorSelectDay;
 			affichageEvent(balise, cal);
 			idPrec = balise;
+			//----Parcour de tous les évènements----//
 		}
 		//----Recoloration du jour sélèctionné----//
-		if(parseInt(eval("sp"+i).innerHTML) == wDate.getDate() 
+		if(parseInt(balise.innerHTML) == wDate.getDate() 
 		&& this.currMonth+1 == wDate.getMonth()
 		&& this.currYear == wDate.getFullYear()
-		&& eval("sp"+i).style.backgroundColor != colorOtherMonth
-		&& eval("sp"+i).style.backgroundColor != colorToday)
-			eval("sp"+i).style.backgroundColor = colorSelectDay;
+		&& balise.style.backgroundColor != colorOtherMonth
+		&& balise.style.backgroundColor != colorToday)
+			balise.style.backgroundColor = colorSelectDay;
 	}
 }
 
@@ -163,56 +171,57 @@ function clickOnCase(id, cal){
 //////////////////////////////////////////////Affichage des evènements///////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function affichageEvent(id, val){
-	//----Définition du jour, du mois et de l'année clicker----//
-	//======>Pas pareil
-	var mw = val.currMonth+1;
-	var yw = val.currYear;
-	//var mw = currM;
-	//var ym = currY;
-	dw = parseInt(eval(id).innerHTML);
-	wDate.setDate(dw);
-	wDate.setMonth(mw); 
-	wDate.setYear(yw);
-	
+
 	var text = "";
-	
 	//----------Affichage de la date clicker-----------//
 	text += "<td>Evènement du ";
-	text += (dw < 10)?"0"+ dw:dw;	//Text prend : la valeur de la case, le jour
-	text += "/";					
-	text += (mw < 10)?"0"+ mw:mw;	//Text prend : valeur du mois
-	text += "/";
-	text += yw;						//Text prend : valeur de l'année
+	text += eval(id).name;
 	text += " :</br>";
 
 	//---------------Affichage du contenu de l'évènement-----------------//
-
 	var textEvent = "";
+	var dateEvent;
 	for(var j=0; j < parseInt(tabEvent.length); j++){
-		for(var k=0; k<5; k++)
+		for(var k=0; k<2; k++)
 		{
-			if(de[j][k] == dw 
-			&& me[j][k] == mw
-			&& ye[j][k] == yw)
-				textEvent += "<table border=1><tr><td align=left><span id=title>" + tabEvent[j][0] + "</span></td><td><span id=content>" + tabEvent[j][1]+ "</span></td></tr></table>";
+			dateEvent = de[j][k] + "/" + me[j][k] +"/"+ ye[j][k];
+			//alert(dateEvent +"=="+ eval(id).name)
+			if(dateEvent == eval(id).name)
+				textEvent += "<table border=1><tr><td align=left><span id=title>" + tabEvent[j][1] + "</span></td><td><span id=content>" + tabEvent[j][2]+ "</span></td></tr></table>";
 		}
-		
 	}
 	
 	text += (textEvent=="")?"Aucun évènement prévu pour le moment..."+"</br>":textEvent;
-	//eval("ev"+i).innerHTML = text; 	//La balise dont l'id vaut 'eve' affiche le text
-	//document.getElementById("calPlace").innerHTML = text;
+	document.getElementById("EventPlace").innerHTML = text;
+}
+
+function affichageNotifEvent(cal, balise){
+	var prevM = (cal.currMonth==0)?11:cal.currMonth-1; //Mois précédent au mois affiché
+	var nextM = (cal.currMonth==11)?0:cal.currMonth+1;
+	var dateEvent;
+	//----Parcour de tous les évènements----//
+	for(var i = 0; i < cal.nbCases; i++){
+		for(var j=0; j < parseInt(tabEvent.length); j++){
+			for(var k=0; k<=1; k++){
+				dateEvent = de[j][k] + "/" + me[j][k] +"/"+ ye[j][k];
+				if(dateEvent == eval("sp"+i).name){
+					if(balise=="sp")eval(balise+i).innerHTML += "<a><br><i style=\"font-size:10px\" class=\"far fa-dot-circle\"></i></br></a>"; //Alort affiché un cercle
+					else eval(balise+i).innerHTML += "<a><i style=\"font-size:10px\" class=\"far fa-dot-circle\"></i></a>";
+				}
+			}
+		}	  
+		//--------------------------------------//
+	}
+
 }
 
 window.onresize = function resize(){
 	windowWidth = parseInt(document.body.clientWidth);
 	if(windowWidth < 768){
-		//alert("tonperetamer");
-		alert(eval("greatTab").style.width);
-		eval("calform").style.width = windowWidth + "px";
+		eval("sp"+i).style.width = windowWidth/7-30+ "px";
 	}
 	else
-		eval("calForm").style.width = windowWidth/21+"px";
+		eval("sp"+i).style.width = windowWidth/21+"px";
 };
 //for (i=0;i<calendarMonth.nbCases;i++)
 /*window.onresize = function resize(){
