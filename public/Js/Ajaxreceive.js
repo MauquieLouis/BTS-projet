4e842e835704faaa2253121f040b5e20c4fb88cb
CHAOUCHI <massi.chaouchi@gmail.com>
2019-04-18 10:18:12 +0200

    table::ByDate ajout commentaires
    table::ByDate("aaaa-mm-jj","aaaa-mm-jj")

--- public/Js/Ajaxreceive.js
+++ public/Js/Ajaxreceive.js
@@ -1,6 +1,8 @@
+//Objet table :acces au contenu de la bdd 'farella'
+
 var table = {
-	retour : "NA",
-	init : function(tableSelected,id){
+	retour : "NA",	//valeur de la table initialiser par table::init(tableSelected[,id])
+	init : function(tableSelected,id){		//initialisation d'une table table::init(tableSelected[,id])
 		var xhReq = new XMLHttpRequest();
 		xhReq.onreadystatechange = function() {
 			if (this.readyState == 4 && (this.status == 200 || this.status == 0)) {
@@ -8,50 +10,50 @@ var table = {
 			}
 		};
 		
-		switch(String(tableSelected)){
+		switch(String(tableSelected)){		//acces a une table en fonction de la table selectionné par l'argument tableSelected
 			case "event":
-				if (id === undefined) {
-					xhReq.open("post","accesbdd/sendevent/undefined",false);		//arg2 url de l'echo de la table SQl recuperé un PHP
+				if (id === undefined) {			
+					xhReq.open("post","accesbdd/sendevent/undefined",false);		//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 				else
 				{
-					xhReq.open("post","accesbdd/sendevent/"+String(id),false);		//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","accesbdd/sendevent/"+String(id),false);		//arg2 :  url de l'echo de la table SQl recuperé un PHP
 				}
 			break;
 
 			case "machine":
 				if (id === undefined) {
-						xhReq.open("post","accesbdd/sendmachine/undefined",false);		//arg2 url de l'echo de la table SQl recuperé un PHP
+						xhReq.open("post","accesbdd/sendmachine/undefined",false);		//arg2 : url de l'echo de la table SQl recuperé un PHP
 					}
 				else
 				{
-					xhReq.open("post","accesbdd/sendmachine/"+String(id),false);	//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","accesbdd/sendmachine/"+String(id),false);	//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 			break;
 			
 			case "user":
 				if (id === undefined) {
-					xhReq.open("post","../../accesbdd/senduser/undefined",false);		//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","../../accesbdd/senduser/undefined",false);		//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 				else
 				{
-					xhReq.open("post","../../accesbdd/senduser/"+String(id),false);	//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","../../accesbdd/senduser/"+String(id),false);	//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 			break;
 
 			case "maintenance":
 				if (id === undefined) {
-					xhReq.open("post","accesbdd/sendmaintenance/undefined",false);		//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","accesbdd/sendmaintenance/undefined",false);		//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 				else
 				{
-					xhReq.open("post","accesbdd/sendmaintenance/"+String(id),false);	//arg2 url de l'echo de la table SQl recuperé un PHP
+					xhReq.open("post","accesbdd/sendmaintenance/"+String(id),false);	//arg2 : url de l'echo de la table SQl recuperé un PHP
 				}
 			break;
 			
 			default:
 				console.log("table::initTable::switch(tableSelected) => DROP TO DEFAULT at AjaxReceive.js ln33");
-				return -1;
+				return -1;			//ERROR : arg tablesSelected inconnu
 			break;
 		}
 		
@@ -72,7 +74,7 @@ var table = {
 
 		return currentUser;
 	},
-	ByDate: function(dateStart,dateEnd){
+	ByDate: function(dateStart,dateEnd){		//table::ByDate("aaaa-mm-jj","aaaa-mm-jj")
 		var xhReq = new XMLHttpRequest();
 		var events;
 		xhReq.onreadystatechange = function() {
